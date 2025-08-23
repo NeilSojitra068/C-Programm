297bf547f55fa93d9ac2f7acf50ab37318828dcd
NeilSojitra068 <neilsojitra01@gmail.com>
2025-08-23 19:50:29 +0530

    Add files via upload

---
+++ Basic_Q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main (){
+    int a,b;
+   printf("Enter No 1 : ");
+   scanf("%d",&a);
+
+   printf("Enter No 2 : ");
+   scanf("%d",&b);
+
+   printf("The Sum Of Numbers :%d \n",a+b);
+         return 0;
+}
