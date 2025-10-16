ff5e072b636fe4cd6d1321d7dabff63b69c24edd
Neil Sojitra CSBS 029 <neilsojitra01@gmail.com>
2025-10-16 15:22:24 +0530

    Update Loop_Q28.c

--- Loop_Q28.c
+++ Loop_Q28.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
-int main(){
-    int n,i;
-    printf("Enter No :");
-    scanf("%d",&n);
-    int sum = 0;
-    for(i=1;i<=n/2;i++){
-        sum=sum+i;
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter No : ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n / 2; i++) {
+        if(n % i == 0) {
+            sum = sum + i;
+        }
     }
-    if(n=sum){
-        printf("%d is Perfect Number",n);
+
+    if(n == sum) {
+        printf("%d is a Perfect Number", n);
+    } else {
+        printf("%d is Not a Perfect Number", n);
     }
-    else printf("%d is Not a Perfect Number ",n);
+
     return 0;
 }
