f863bdfbbc2453b0bef6e6294f7962a417fb3f10
Neil Sojitra CSBS 029 <neilsojitra01@gmail.com>
2025-10-16 15:29:54 +0530

    Update Loop_Q29.c

--- Loop_Q29.c
+++ Loop_Q29.c
@@ -3,6 +3,10 @@ int main(){
     int n,i;
     printf("Enter No :");
     scanf("%d",&n);
+   if (n==0 || n==1 ) {
+        printf(" %d Is Not A Prime Number ",n);
+        return 0;
+        }
     if (n==2) {
         printf(" %d Is Prime Number ",n);
         return 0;
