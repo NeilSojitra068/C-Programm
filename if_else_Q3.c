0394b7e02b742e1fcd68cc545447edb71a1f7dda
Neil Sojitra CSBS 029 <neilsojitra01@gmail.com>
2025-11-01 00:29:50 +0530

    Update if_else_Q3.c

--- if_else_Q3.c
+++ if_else_Q3.c
@@ -10,15 +10,16 @@ int main(){
     scanf("%f",&a);
 
     if(a>10000){
-        printf("Net Salary %f \n :",a+0.1*a-0.03*a);
+        printf("Net Salary :%f \n",a+0.1*a-0.03*a);
     }
 
     else if (a>5000){
-        printf("Net Salary %f \n :",a+0.07*a-0.02*a);
+        printf("Net Salary :%f \n",a+0.07*a-0.02*a);
     }
 
     else
         printf("Not Applicable");
 
 return 0;
+
 }
