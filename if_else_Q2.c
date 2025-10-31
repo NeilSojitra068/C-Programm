ba050c042e59ea0d980c688e81f4677645876167
Neil Sojitra CSBS 029 <neilsojitra01@gmail.com>
2025-11-01 00:21:14 +0530

    Refactor to find largest and smallest values

--- if_else_Q2.c
+++ if_else_Q2.c
@@ -1,45 +1,32 @@
-// Q-2)Find The Largest And Smallest Value Among 3 Numbers
-#include<stdio.h>
+// Q-2) Find The Largest And Smallest Value Among 3 Numbers
+#include <stdio.h>
 
-int main(){
-    int a,b,c;
-    printf("Enter 1st Number :");
-    scanf("%d",&a);
-    printf("Enter 2nd Number :");
-    scanf("%d",&b);
-    printf("Enter 3rd Number :");
-    scanf("%d",&c);
+int main() {
+    int a, b, c;
+    printf("Enter 1st Number: ");
+    scanf("%d", &a);
+    printf("Enter 2nd Number: ");
+    scanf("%d", &b);
+    printf("Enter 3rd Number: ");
+    scanf("%d", &c);
 
-    if(a>b && b>c){
-        printf("%d is Largest\n",a);
-        printf("%d is smallest\n",c);
-    }
+    int largest = a;
+    int smallest = a;
 
-    else if (a>c && c>b){
-        printf("%d is Largest\n",a);
-        printf("%d is smallest\n",b);
-    }
+    
+    if (b > largest)
+        largest = b;
+    if (c > largest)
+        largest = c;
 
-    else if (b>c && c>a){
-        printf("%d is Largest\n",b);
-        printf("%d is smallest\n",a);
-    }
+    
+    if (b < smallest)
+        smallest = b;
+    if (c < smallest)
+        smallest = c;
 
-    else if (b>a && a>c){
-        printf("%d is Largest\n",b);
-        printf("%d is smallest\n",c);
-    }
-    else if (c>b && b>a){
-        printf("%d is Largest\n",c);
-        printf("%d is smallest\n",a);
-    }
-
-    else {
-
-        printf("%d is Largest\n",c);
-        printf("%d is smallest\n",b);
-    }
+    printf("%d is Largest\n", largest);
+    printf("%d is Smallest\n", smallest);
 
     return 0;
-
 }
