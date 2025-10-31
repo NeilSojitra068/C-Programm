51b5ac5bc39d8a52dc186bb8998ccdc91ce6c652
Neil Sojitra CSBS 029 <neilsojitra01@gmail.com>
2025-11-01 00:43:13 +0530

    Refactor output format for net sales calculation

--- if_else_Q5.c
+++ if_else_Q5.c
@@ -10,15 +10,16 @@ int main(){
     scanf("%f",&a);
 
     if(a>20000){
-        printf("Net Sales %f \n :",a-0.15*a);
+        printf("Net Sales :%f \n",a-0.15*a);
     }
 
     else if (a>10000){
-        printf("Net Sales %f \n :",a-0.1*a);
+        printf("Net Sales  :%f \n",a-0.1*a);
     }
 
     else
-        printf("Net Sales %f \n :",a-0.05*a);
+        printf("Net Sales :%f \n",a-0.05*a);
 return 0;
 }
+
 
